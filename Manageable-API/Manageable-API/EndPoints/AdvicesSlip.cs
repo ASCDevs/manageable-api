b999c5bf010d2dc2ed370fc9018a1945e1fbdc2e
Alexandre Cavalcante <ale.santos49@gmail.com>
2023-08-21 21:06:45 -0300

    Base project to start

---
+++ Manageable-API/Manageable-API/EndPoints/AdvicesSlip.cs
@@ -0,0 +1,23 @@
+﻿using Carter;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Manageable_API.EndPoints
+{
+    public class AdvicesSlip : CarterModule
+    {
+        public AdvicesSlip() : base("/theadvisor")
+        {
+            
+        }
+
+        public override void AddRoutes(IEndpointRouteBuilder app)
+        {
+            //https://api.adviceslip.com/#endpoint-random
+            app.MapGet("/help", () =>
+            {
+                return Results.Ok(new { descricao = "Tras conselhos diversos", data_informacao = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss") });
+            });
+        }
+    }
+}
---
+++ Manageable-API/Manageable-API/EndPoints/FunTranslationsEndPoints.cs
@@ -0,0 +1,20 @@
+﻿using Carter;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Manageable_API.EndPoints
+{
+    public class FunTranslationsEndPoints : CarterModule
+    {
+        public FunTranslationsEndPoints() : base("/translatorfun") { }
+
+        public override void AddRoutes(IEndpointRouteBuilder app)
+        {
+            // https://funtranslations.com/api/mandalorian
+            app.MapGet("/help", () =>
+            {
+                return Results.Ok(new { descricao = "Faz traduções de diversos tipos doidos.", data_informacao = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss") });
+            });
+        }
+    }
+}
